2e7e9fd91b2105f97e6427f555f350cd6ec4a7e6
EinerSanchez <eidavisaqui05@mail.com>
2024-08-01 18:41:44 -0500

    Cargando codigo de poker ise modificacion y añadi el panel de opciones

--- poker.php
+++ poker.php
@@ -1,12 +1,12 @@
 <?php
 //declarar funcion de repartircartas
 function repartirCartas() {
-    // Definir el mazo de cartas
+   
     $mazo = array(
-        "As de corazones", "2 de corazones", "3 de corazones", "4 de corazones", "5 de corazones", "6 de corazones", "7 de corazones", "8 de corazones", "9 de corazones", "10 de corazones", "Jota de corazones", "Reina de corazones", "Rey de corazones",
-        "As de diamantes", "2 de diamantes", "3 de diamantes", "4 de diamantes", "5 de diamantes", "6 de diamantes", "7 de diamantes", "8 de diamantes", "9 de diamantes", "10 de diamantes", "Jota de diamantes", "Reina de diamantes", "Rey de diamantes",
-        "As de picas", "2 de picas", "3 de picas", "4 de picas", "5 de picas", "6 de picas", "7 de picas", "8 de picas", "9 de picas", "10 de picas", "Jota de picas", "Reina de picas", "Rey de picas",
-        "As de tréboles", "2 de tréboles", "3 de tréboles", "4 de tréboles", "5 de tréboles", "6 de tréboles", "7 de tréboles", "8 de tréboles", "9 de tréboles", "10 de tréboles", "Jota de tréboles", "Reina de tréboles", "Rey de tréboles"
+        "As de ♥", "2 de ♥", "3 de ♥", "4 de ♥", "5 de ♥", "6 de ♥", "7 de ♥", "8 de ♥", "9 de ♥", "10 de ♥", "Jota de ♥", "Reina de ♥", "Rey de ♥",
+        "As de ♦", "2 de ♦", "3 de ♦", "4 de ♦", "5 de ♦", "6 de ♦", "7 de ♦", "8 de ♦", "9 de ♦", "10 de ♦", "Jota de ♦", "Reina de ♦", "Rey de ♦",
+        "As de ♠", "2 de ♠", "3 de ♠", "4 de ♠", "5 de ♠", "6 de ♠", "7 de ♠", "8 de ♠", "9 de ♠", "10 de ♠", "Jota de ♠", "Reina de ♠", "Rey de ♠",
+        "As de ♣", "2 de ♣", "3 de ♣", "4 de ♣", "5 de ♣", "6 de ♣", "7 de ♣", "8 de ♣", "9 de ♣", "10 de ♣", "Jota de ♣", "Reina de ♣", "Rey de ♣"
     );
 
     // Repartir 5 cartas aleatorias
@@ -16,16 +16,17 @@ function repartirCartas() {
         $cartas[] = $carta;
         unset($mazo[array_search($carta, $mazo)]);
     }
-
+//retornar las cartas
     return $cartas;
 }
-//funcion de mostrar cartas 
-function mostrarCartas($cartas) {
+//declaro funcion de mostrar cartas 
+function mostrarCartas($cartas) { 
+    echo"Tu mano de carta es:\n";
     foreach ($cartas as $carta) {
         echo "$carta\n";
     }
 }
-//funcion de evaluarmano
+// declaro funcion de evaluarmano
 function evaluarMano($cartas) {
     // Evaluación básica de la mano (sin considerar el orden de las cartas)
     $valores = array();
@@ -35,24 +36,23 @@ function evaluarMano($cartas) {
         $valores[] = $valor;
         $palos[] = $palo;
     }
-
     // Verificar si es un par
     if (count(array_unique($valores)) == 4) {
-        echo "Mano: Par\n";
+        echo "La mejor combinacion es: Par\n";
     }
-    // Verificar si es un trío
+ 
     elseif (count(array_unique($valores)) == 3) {
         echo "La mejor combinacion es: Trío\n";
     }
-    // Verificar si es una escalera
+
     elseif (in_array("As", $valores) && in_array("2", $valores) && in_array("3", $valores) && in_array("4", $valores) && in_array("5", $valores)) {
         echo "La mejor combinacion es: Escalera\n";
     }
-    // Verificar si es un color
+  
     elseif (count(array_unique($palos)) == 1) {
         echo "La mejor combinacion es: Color\n";
     }
-    // Verificar si es una escalera de color
+   
     elseif (in_array("As", $valores) && in_array("2", $valores) && in_array("3", $valores) && in_array("4", $valores) && in_array("5", $valores) && count(array_unique($palos)) == 1) {
         echo "La mejor combinacion es: Escalera de color\n";
     }
@@ -60,8 +60,32 @@ function evaluarMano($cartas) {
         echo "La mejor combinacion es: Carta alta\n";
     }
 }
-//ejecucion de todas las funciones
+//ejecucion 
 $cartas = repartirCartas();
 mostrarCartas($cartas);
 evaluarMano($cartas);
+
+$repartir = true; 
+
+while (true) {
+    echo "1. Quieres volver a repartit cartas\n";
+    echo "2. Salir\n";
+    $opcion = readline("Ingrese una opción: ");
+    
+    switch ($opcion) {
+      case 1:
+        $cartas = repartirCartas();
+        mostrarCartas($cartas);
+        evaluarMano($cartas);
+        break;
+      case 2:
+        echo "Chauuu";
+        exit;
+        default: 
+        echo "¡opcion invalida!";
+        exit;
+        break;
+    }
+  }
+  
 ?>
